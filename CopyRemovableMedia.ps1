59cb09eba1bfd249f3864abdd54cdcc15ceb2839
fpdavis <github+pdavis@dotcomsensei.com>
2020-08-15 14:49:45 -0500

    Update CopyRemovableMedia.ps1
    Added autoupdate

--- CopyRemovableMedia.ps1
+++ CopyRemovableMedia.ps1
@@ -5,14 +5,28 @@ Param (
     [switch]$SubfolderSearch, 
     [switch]$CopyToPopulatedDirectory,     
     [switch]$Quiet, 
-    [switch]$BypassEject, 
+    [switch]$NoEject, 
     [switch]$ExitAfterCopy, 
+    [switch]$NoAutoUpdate, 
     $Source = '', 
     $Destination = '.', 
     [goVerbosityEnum]$Verbosity = [goVerbosityEnum]::Verbose
     )
 
-$Version = 1.0
+$Version = 1.2
+$Destination = 'F:\Data\Disk Archives\CD-DVD Archive'
+
+
+Add-Type -TypeDefinition @"
+    public enum goVerbosityEnum {
+    Critical,
+    Error,
+    Warning,
+    Information,
+    Verbose,
+    Debug
+}
+"@
 
 Function Main {
                                                                                                                                                                                                                                                                                                            
@@ -28,7 +42,7 @@ Function Main {
      }
 
      CheckForUpdate
-
+     exit
      MessageLog "`nInsert media to be copied... Ctrl-C to cancel"
 
     while ($true) {    
@@ -46,30 +60,24 @@ Function Main {
     }
 }
 
-Add-Type -TypeDefinition @"
-    public enum goVerbosityEnum {
-    Critical,
-    Error,
-    Warning,
-    Information,
-    Verbose,
-    Debug
-}
-"@
 
 Function DisplayHelp {
-    MessageLog "`nNAME"
-    MessageLog "`tCopyRemovableMedia.ps1"
 
+    $Hash = CalculateHash $MyInvocation.ScriptName
+
+    MessageLog ("`nNAME")
+    MessageLog ("`tCopyRemovableMedia.ps1 (Version " + $Version + ", Hash: " + $Hash.Hash + ")")
+    
 	MessageLog "`nSYNTAX"
-    MessageLog "`tCopyRemovableMedia"
-    MessageLog "`t`t[-Help]`n"
+    MessageLog ("`t" + $MyInvocation.ScriptName.Replace((Split-Path $MyInvocation.ScriptName),'').TrimStart('\') + "`n")
+    MessageLog "`t`t[-Help]"
     MessageLog "`t`t[-UseNewestFolder]"
     MessageLog "`t`t[-SubfolderSearch]"
     MessageLog "`t`t[-CopyToPopulatedDirectory]"    
     MessageLog "`t`t[-Quiet]"
-    MessageLog "`t`t[-BypassEject]"    
+    MessageLog "`t`t[-NoEject]"    
     MessageLog "`t`t[-ExitAfterCopy]"    
+    MessageLog "`t`t[-NoAutoUpdate]"      
     MessageLog "`t`t[-Confirm]"  
     MessageLog "`t`t[-WhatIf]"  
     MessageLog "`t`t[-Source 'c:\Your\Source\Directory']"
@@ -104,10 +112,11 @@ Function DisplayHelp {
     MessageLog "`t`t`t                         to copy files into an existing directory that already contains files."
     MessageLog "`t`t`t                         This paramater should be used sparingly and with caution."
     MessageLog "`t`t                   Quiet - Refrains from playing a chime when the copy is complete."
-    MessageLog "`t`t             BypassEject - Refrains from ejecting the optical drive after a copy is complete."    
+    MessageLog "`t`t                   Eject - Refrains from ejecting the optical drive after a copy is complete."    
     MessageLog "`t`t           ExitAfterCopy - If this parameter is not specified the program will do multiple copies"
     MessageLog "`t`t`t                         one after another. If specified the program will exit after the first"
     MessageLog "`t`t`t                         copy is made."
+    MessageLog "`t`t            NoAutoUpdate - Skips checking for an update online."
     MessageLog "`t`t                 Confirm - The SupportsShouldProcess argument adds Confirm and WhatIf parameters to"
     MessageLog "`t`t`t                         the function. The Confirm parameter prompts the user before it performs"
     MessageLog "`t`t`t                         a copy (and before it creates the destination directory if necessary)."
@@ -146,7 +155,7 @@ Function DisplayHelp {
     # 4 -- Network Drive
     # 5 -- Compact Disc
     # 6 -- Ram Disk
-    
+    exit
     MessageLog "`nREMOVABLE(2)/OPTICAL(5) DRIVE INFORMATION"    
     Get-CimInstance Win32_LogicalDisk | ?{ $_.DriveType -eq 2 -or $_.DriveType -eq 5} 
     
@@ -292,7 +301,7 @@ Function CopyProcess {
 
     start-sleep (1); 
 
-    if (-Not $BypassEject) {
+    if (-Not $NoEject) {
         (New-Object -com "WMPlayer.OCX.7").cdromcollection.item(0).eject()
     }
 
@@ -355,10 +364,64 @@ Function PlayCompletionSound {
 
 Function CheckForUpdate {
 
-$Script = (New-Object System.Net.WebClient).Downloadstring('')
+    if ($NoAutoUpdate) { return }
 
+    MessageLog ("Version: " + $Version)
+    MessageLog ("Checking for updates...")
+
+    $LatestVersion = (New-Object System.Net.WebClient).Downloadstring('https://raw.githubusercontent.com/fpdavis/CopyRemovableMedia/master/Version.txt')
+    $LatestVersion = $LatestVersion.Trim().Split(":")
+    if ($Version -lt $LatestVersion[0]) {
+        MessageLog ("Updating from version " + $Version + " to version " + $LatestVersion[0])
+        
+        #$Script = (New-Object System.Net.WebClient).Downloadstring('https://raw.githubusercontent.com/fpdavis/CopyRemovableMedia/master/CopyRemovableMedia.ps1')
+        $LatestVersionPath = $MyInvocation.ScriptName.Replace(".ps1", "_" + $LatestVersion[0] + ".ps1")
+        $Script = (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/fpdavis/CopyRemovableMedia/master/CopyRemovableMedia.ps1', $LatestVersionPath)
+        #Set-Content -Path $LatestVersionPath -Value $Script
+
+        $Hash = CalculateHash($LatestVersionPath)
+
+        MessageLog ("Verifying Hash (" + $Hash.Hash + ")") [goVerbosityEnum]::Verbose
+        if ($Hash.Hash -eq $LatestVersion[1].ToUpper()) {
+            MessageLog ("Hash match") [goVerbosityEnum]::Verbose
+            $OldVersionPath = $MyInvocation.ScriptName.Replace(".ps1", "_" + $Version + ".ps1")
+
+            MessageLog ("Archiving current version as " + $OldVersionPath)
+            Rename-Item -Path $MyInvocation.ScriptName -NewName $OldVersionPath
+            Rename-Item -Path $LatestVersionPath -NewName $MyInvocation.ScriptName
+
+            MessageLog ("Restarting new version...") [goVerbosityEnum]::Warning
+
+            $Paramaters = " -NoAutoUpdate "
+            foreach ($Argument in $PSBoundParameters.GetEnumerator()) {
+                $Paramaters += " -" + $Argument.Key
+
+                if ($Argument.Value -ne $true) {
+                    $Paramaters += " '" + $Argument.Value + "'"
+                }
+            }
+            & $MyInvocation.ScriptName + $Paramaters
+            exit
+        }
+        else {
+           MessageLog ("Hash did not match $($LatestVersion[1]), skipping update.") [goVerbosityEnum]::Error
+           Remove-Item $LatestVersionPath
+        }
+
+
+    }
+    else {
+       MessageLog ("Currently on latest version.")
+    }
+
+    exit
 }
 
-Main
+Function CalculateHash($Path) {
 
- 
+    Get-FileHash  -Path $Path -Algorithm MD5
+         
+}
+
+
+Main
